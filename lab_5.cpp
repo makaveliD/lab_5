6e3d2729bdc4de7482e2737694306a0737270f19
Vova Zabalotny <vovazabalotny@gmail.com>
2022-12-30 19:48:36 +0200

    add lab

---
+++ lab_5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int isPalindrome(int number)
+{
+    int reverse = 0;
+    for (int i = number; i > 0; i /= 10)
+    {
+        reverse = reverse * 10 + i % 10;
+    }
+
+    return number == reverse;
+}
+
+int main()
+{
+    for (int i = 0; i <= 1000; i++)
+    {
+        if (isPalindrome(i)) {
+            cout << i << endl;
+        }
+    }
+}
